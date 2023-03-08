ca98629be16e7fded5b04fbe8da09bd36ab499d0
annushka <annikolaeva0211@mail.ru>
2023-03-08 10:39:43 +0300

    add fourth task

--- Program.cs
+++ Program.cs
@@ -70,3 +70,21 @@ else
 {
     Console.WriteLine($"Нет");
 }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int N;
+
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine(), out N);
+
+int x = 2;
+
+if (N > 1)
+{
+    while (x <= N)
+    {
+        Console.Write(x + " ");
+        x = x + 2;
+    }
+}
